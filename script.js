2a446583d2857a2d08695da8aee7e2a1c8241c8d
Kodaaaa1 <gabrieldias.gs2@gmail.com>
2026-03-20 18:00:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let display = document.getElementById("display");
 
 function appendValue(value) {
   if (display.innerText === "0") {
-    display.innerText = value;
-  } else {
-    display.innerText += value;
+    display.innerText = "";
   }
+
+  display.innerText += value;
 }
 
 function clearDisplay() {
